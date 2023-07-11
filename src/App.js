b8ea495fbfc9abfd537a5dc3154e22238d1300c4
yashvachhan1 <yashvachhani96@gmail.com>
2023-07-11 12:55:08 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./mycomponents/Header"; 
+import {Todos} from "./mycomponents/Todos";
+import {Footer} from "./mycomponents/Footer";
+function App() {
+let  myvariable = 345;
+  return (
+    <>
+<Header/>
+<Todos/>
+<Footer/>
+
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/mycomponents/Todo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Todo = () => {
+  return (
+    <div>
+      todo works
+    </div>
+  )
+}
+
+
